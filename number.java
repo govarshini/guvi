8f677e7331fe0322a7326e47589873f95a8111e2
govarshini <govarshiniarumugam@gmail.com>
2017-06-09 17:56:35 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class number
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a==0)
+		System.out.println("the number is constant");
+		if(a>0)
+		System.out.println("the number is postive");
+		if(a<0)
+		System.out.println("the number is negative");
+	}
+	}
