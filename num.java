639525883248e5bd4d6a96c03f626dd93e0e08ad
govarshini <govarshiniarumugam@gmail.com>
2017-06-09 18:20:38 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+class num
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a%2==0)
+		System.out.println("the number is even");
+		else
+			System.out.println("the number is odd");
+		
+	}	
+	}
